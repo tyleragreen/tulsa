ee4a69aada09c909953850987c6a39781c1dc163
Tyler Green <tgreen8091@gmail.com>
2023-07-31 15:29:59 -0400

    Update integration test to actually do a POST call

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,3 @@ pub mod api;
 pub mod fetcher;
 pub mod model;
 pub mod scheduler;
-
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,17 +1,53 @@
 #[cfg(test)]
 mod tests {
-    use gtfs_realtime_rust::add;
+    use gtfs_realtime_rust::api;
     use gtfs_realtime_rust::scheduler;
+    use reqwest::blocking::Client;
+    use serde_json::json;
     use std::sync::mpsc;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(add(2, 2), 4);
-    }
+    use std::thread;
+    use std::time::Duration;
+    use tokio::runtime::Builder;
 
     #[test]
     fn integration() {
-        let (_, receiver) = mpsc::channel();
+        let (sender, receiver) = mpsc::channel();
         scheduler::init(receiver);
+
+        let address: &str = "0.0.0.0:3000";
+        thread::spawn(move || {
+            let runtime = Builder::new_multi_thread().enable_io().build().unwrap();
+
+            runtime.block_on(async {
+                axum::Server::bind(&address.parse().unwrap())
+                    .serve(api::app(sender).into_make_service())
+                    .await
+                    .unwrap();
+            });
+        });
+
+        thread::sleep(Duration::from_secs(1));
+
+        let client = Client::new();
+        let data = json!({
+            "name": "MTA",
+            "frequency": 5,
+            "url": "no_url",
+            "headers": {
+                "x-api-key": "no_key"
+            },
+        });
+
+        let response = client.post("http://localhost:3000/feed").json(&data).send();
+
+        match response {
+            Ok(response) => {
+                assert_eq!(response.status(), 201);
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                assert!(false);
+            }
+        }
     }
 }
