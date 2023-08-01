5ce959b040043e07f1df1efda430b57a2e4a4208
Tyler Green <tgreen8091@gmail.com>
2023-08-01 10:54:53 -0400

    Make the scheduler test a bit more meaningful

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,42 +1,74 @@
 #[cfg(test)]
 mod tests {
     use gtfs_realtime_rust::api;
-    use gtfs_realtime_rust::scheduler;
     use gtfs_realtime_rust::model::AsyncTask;
+    use gtfs_realtime_rust::scheduler;
     use reqwest::blocking::Client;
     use serde_json::json;
+    use std::fs::File;
+    use std::fs::OpenOptions;
+    use std::io::prelude::*;
+    use std::process::Command;
     use std::sync::mpsc;
     use std::thread;
     use std::time::Duration;
-    //use std::fs::File;
-    //use std::io::prelude::*;
     use tokio::runtime::Builder;
+
+    fn confirm_wc(file_path: &str, expected: i32) {
+        let output = Command::new("wc")
+            .arg(file_path)
+            .output()
+            .expect("failed to execute process");
+
+        if output.status.success() {
+            let output_str = String::from_utf8_lossy(&output.stdout);
+            let output_trimmed = output_str.trim();
+            let output_vec: Vec<&str> = output_trimmed.split(" ").collect();
+            let output_num: i32 = output_vec[0].parse().unwrap();
+            assert_eq!(output_num, expected);
+        } else {
+            assert!(false);
+        }
+    }
 
     #[test]
     fn scheduler() {
         let (sender, receiver) = mpsc::channel();
         scheduler::init(receiver);
 
-        let task = AsyncTask::new(1, async {
-            println!("Hello from async task");
-            //let mut file = File::create("test.txt").unwrap();
+        let file_name: &str = "/tmp/rust_test_output.txt";
 
-            //file.write_all(b"Hello, world!").unwrap();
+        // Clear the file and ensure it exists
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(file_name)
+            .unwrap();
+        file.write_all(b"").unwrap();
 
-            //thread::sleep(Duration::from_millis(1000));
+        let task = AsyncTask::new(1, async move {
+            let duration = Duration::from_millis(100);
+            let mut interval = tokio::time::interval(duration);
+            let mut file = File::create(file_name).unwrap();
 
-            //file.write_all(b"Goodbye, world!").unwrap();
+            for i in 0..5 {
+                interval.tick().await;
+                file.write_all(i.to_string().as_bytes()).unwrap();
+                file.write_all(b"\n").unwrap();
+            }
         });
 
-        //thread::sleep(Duration::from_secs(2));
+        // File should still be empty before the send the task to the scheduler
+        confirm_wc(file_name, 0);
         match sender.send(task) {
-            Ok(_) => {
-                assert!(true);
-            }
-            Err(_) => {
-                assert!(false);
-            }
+            Ok(_) => assert!(true),
+            Err(_) => assert!(false),
         }
+
+        // Wait for the task to finish and then confirm the file has the correct contents
+        thread::sleep(Duration::from_millis(550));
+        confirm_wc(file_name, 5);
     }
 
     #[test]
