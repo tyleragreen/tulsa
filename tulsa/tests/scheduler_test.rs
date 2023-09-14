1bee6f581dfbc4b87ef006cca794e30cbf155e5b
Tyler Green <tgreen8091@gmail.com>
2023-09-14 11:39:44 -0400

    Small cleanup to tulsa tests

--- tulsa/tests/scheduler_test.rs
+++ tulsa/tests/scheduler_test.rs
@@ -12,7 +12,7 @@ mod tests {
 
     use tulsa::{AsyncTask, Scheduler, SyncTask};
 
-    fn confirm_wc(file_path: &str, expected: i32) {
+    fn wc(file_path: &str) -> i32 {
         let output = Command::new("wc")
             .arg(file_path)
             .output()
@@ -23,20 +23,21 @@ mod tests {
             let output_trimmed = output_str.trim();
             let output_vec: Vec<&str> = output_trimmed.split(" ").collect();
             let output_num: i32 = output_vec[0].parse().unwrap();
-            assert_eq!(output_num, expected);
+            output_num
         } else {
-            assert!(false);
+            panic!("failed to execute wc successful");
         }
     }
 
     fn touch(file_name: &str) {
-        let mut file = OpenOptions::new()
+        OpenOptions::new()
             .write(true)
             .create(true)
             .truncate(true)
             .open(file_name)
+            .unwrap()
+            .write_all(b"")
             .unwrap();
-        file.write_all(b"").unwrap();
     }
 
     fn create_async_task(id: usize, file_name: &'static str, millis: u64) -> AsyncTask {
@@ -52,73 +53,6 @@ mod tests {
                 file.write_all(b"\n").unwrap();
             }
         })
-    }
-
-    #[test]
-    fn async_scheduler_create() {
-        let (sender, receiver): (Sender<AsyncTask>, Receiver<AsyncTask>) = mpsc::channel();
-        Scheduler::new(receiver).run();
-
-        static FILE_NAME: &'static str = "/tmp/tulsa_async_1.txt";
-
-        // Clear the file and ensure it exists
-        touch(FILE_NAME);
-
-        let task = create_async_task(1, FILE_NAME, 100);
-
-        // File should still be empty before the send the task to the scheduler
-        confirm_wc(FILE_NAME, 0);
-        match sender.send(task) {
-            Ok(_) => assert!(true),
-            Err(e) => {
-                eprintln!("{}", e);
-                assert!(false)
-            }
-        }
-
-        // Wait for the task to finish and then confirm the file has the correct contents
-        thread::sleep(Duration::from_millis(550));
-        confirm_wc(FILE_NAME, 6);
-    }
-
-    #[test]
-    fn async_scheduler_delete() {
-        let (sender, receiver): (Sender<AsyncTask>, Receiver<AsyncTask>) = mpsc::channel();
-        Scheduler::new(receiver).run();
-
-        let task_id: usize = 2;
-        static FILE_NAME: &'static str = "/tmp/tulsa_async_2.txt";
-
-        // Clear the file and ensure it exists
-        touch(FILE_NAME);
-
-        let task = create_async_task(task_id, FILE_NAME, 100);
-
-        // File should still be empty before the send the task to the scheduler
-        confirm_wc(FILE_NAME, 0);
-        match sender.send(task) {
-            Ok(_) => assert!(true),
-            Err(e) => {
-                eprintln!("{}", e);
-                assert!(false)
-            }
-        }
-
-        // Wait for the task to finish and then confirm the file has the correct contents
-        thread::sleep(Duration::from_millis(550));
-
-        confirm_wc(FILE_NAME, 6);
-        let task = AsyncTask::stop(task_id);
-        match sender.send(task) {
-            Ok(_) => assert!(true),
-            Err(e) => {
-                eprintln!("{}", e);
-                assert!(false)
-            }
-        }
-
-        thread::sleep(Duration::from_millis(550));
-        confirm_wc(FILE_NAME, 6);
     }
 
     fn create_sync_task(id: usize, file_name: &'static str, millis: u64) -> SyncTask {
@@ -137,6 +71,73 @@ mod tests {
     }
 
     #[test]
+    fn async_scheduler_create() {
+        let (sender, receiver): (Sender<AsyncTask>, Receiver<AsyncTask>) = mpsc::channel();
+        Scheduler::new(receiver).run();
+
+        static FILE_NAME: &'static str = "/tmp/tulsa_async_1.txt";
+
+        // Clear the file and ensure it exists
+        touch(FILE_NAME);
+
+        let task = create_async_task(1, FILE_NAME, 100);
+
+        // File should still be empty before the send the task to the scheduler
+        assert_eq!(wc(FILE_NAME), 0);
+        match sender.send(task) {
+            Ok(_) => assert!(true),
+            Err(e) => {
+                eprintln!("{}", e);
+                assert!(false)
+            }
+        }
+
+        // Wait for the task to finish and then confirm the file has the correct contents
+        thread::sleep(Duration::from_millis(550));
+        assert_eq!(wc(FILE_NAME), 6);
+    }
+
+    #[test]
+    fn async_scheduler_delete() {
+        let (sender, receiver): (Sender<AsyncTask>, Receiver<AsyncTask>) = mpsc::channel();
+        Scheduler::new(receiver).run();
+
+        let task_id: usize = 2;
+        static FILE_NAME: &'static str = "/tmp/tulsa_async_2.txt";
+
+        // Clear the file and ensure it exists
+        touch(FILE_NAME);
+
+        let task = create_async_task(task_id, FILE_NAME, 100);
+
+        // File should still be empty before the send the task to the scheduler
+        assert_eq!(wc(FILE_NAME), 0);
+        match sender.send(task) {
+            Ok(_) => assert!(true),
+            Err(e) => {
+                eprintln!("{}", e);
+                assert!(false)
+            }
+        }
+
+        // Wait for the task to finish and then confirm the file has the correct contents
+        thread::sleep(Duration::from_millis(550));
+
+        assert_eq!(wc(FILE_NAME), 6);
+        let task = AsyncTask::stop(task_id);
+        match sender.send(task) {
+            Ok(_) => assert!(true),
+            Err(e) => {
+                eprintln!("{}", e);
+                assert!(false)
+            }
+        }
+
+        thread::sleep(Duration::from_millis(550));
+        assert_eq!(wc(FILE_NAME), 6);
+    }
+
+    #[test]
     fn sync_scheduler_create() {
         let (sender, receiver): (Sender<SyncTask>, Receiver<SyncTask>) = mpsc::channel();
         Scheduler::new(receiver).run();
@@ -149,7 +150,7 @@ mod tests {
         let task = create_sync_task(1, FILE_NAME, 100);
 
         // File should still be empty before the send the task to the scheduler
-        confirm_wc(FILE_NAME, 0);
+        assert_eq!(wc(FILE_NAME), 0);
         match sender.send(task) {
             Ok(_) => assert!(true),
             Err(e) => {
@@ -160,7 +161,7 @@ mod tests {
 
         // Wait for the task to finish and then confirm the file has the correct contents
         thread::sleep(Duration::from_millis(550));
-        confirm_wc(FILE_NAME, 6);
+        assert_eq!(wc(FILE_NAME), 6);
     }
 
     #[test]
@@ -177,7 +178,7 @@ mod tests {
         let task = create_sync_task(task_id, FILE_NAME, 100);
 
         // File should still be empty before the send the task to the scheduler
-        confirm_wc(FILE_NAME, 0);
+        assert_eq!(wc(FILE_NAME), 0);
         match sender.send(task) {
             Ok(_) => assert!(true),
             Err(e) => {
@@ -189,7 +190,7 @@ mod tests {
         // Wait for the task to finish and then confirm the file has the correct contents
         thread::sleep(Duration::from_millis(550));
 
-        confirm_wc(FILE_NAME, 6);
+        assert_eq!(wc(FILE_NAME), 6);
         let task = SyncTask::stop(task_id);
         match sender.send(task) {
             Ok(_) => assert!(true),
@@ -200,6 +201,6 @@ mod tests {
         }
 
         thread::sleep(Duration::from_millis(550));
-        confirm_wc(FILE_NAME, 6);
+        assert_eq!(wc(FILE_NAME), 6);
     }
 }
