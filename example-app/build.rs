05e170b6c8623226ea5a9dbe0dc09626dc674312
Tyler Green <tgreen8091@gmail.com>
2023-09-15 15:45:26 -0400

    Add prost-build to feature flag

--- example-app/build.rs
+++ example-app/build.rs
@@ -1,5 +1,13 @@
+#[cfg(not(feature = "use_dependencies"))]
 include!("src/deps/prost_build/mod.rs");
 
+#[cfg(feature = "use_dependencies")]
+extern crate prost_build;
+
 fn main() {
+    #[cfg(not(feature = "use_dependencies"))]
     compile_protos(&["src/proto/gtfs-realtime.proto"], &["src/"]).unwrap();
+
+    #[cfg(feature = "use_dependencies")]
+    prost_build::compile_protos(&["src/proto/gtfs-realtime.proto"], &["src/"]).unwrap();
 }
